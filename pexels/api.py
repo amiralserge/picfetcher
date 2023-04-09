2b6a7e7b286dcf1b73f3d500caa7fd5b10e5ed36
amiralserge <kossononserge@yahoo.fr>
2023-04-09 15:56:10 -0400

    Improve Endpoint implementation

--- pexels/api.py
+++ pexels/api.py
@@ -1,40 +1,53 @@
 import requests
+import re
+
+from typing import (Any, Dict, List)
 
 
 class Endpoint:
     base_url = "https://api.pexels.com/v1/"
     api_path = None
-    path_params_keys = []
+    _url_path_param_regex = re.compile(r"\{(\w+)\}")
     _default_headers = {
         'Accept-Content': 'application/json'
     }
+
 
     def __init__(self, apli_key):
         self.session = requests.session()
         self.api_key = apli_key
 
-    def request(self, method, url, **kwargs):
-        headers = kwargs.pop('headers', None) or {}
+    def get(self, **kwargs) -> Any:
+        kwargs.pop('url', None)
+        url_params = kwargs.pop('params', {})
+        path_params = self._remove_path_params(url_params)
+        url = self._get_url(**path_params)
+        return self.request('get', url=url, params=url_params, **kwargs)
+
+    @classmethod
+    def _remove_path_params(cls, params: Dict) -> Dict:
+        return {key: params.pop(key) for key in cls._get_path_params_keys(cls.api_path) if key in params}
+    
+    @classmethod
+    def _get_url(cls, **path_params) -> str:
+        url = f'{cls.base_url}{cls.api_path}'
+        params_kw = {k: path_params.get(k) for k in cls._get_path_params_keys(url) if k in path_params}
+        return url.format(**params_kw) if params_kw else url
+
+    @classmethod
+    def _get_path_params_keys(cls, url) -> List[str]:
+        return cls._url_path_param_regex.findall(url)
+
+    def request(self, method, url, **kwargs) -> Any:
+        headers = dict(self._default_headers)
+        headers.update(kwargs.pop('headers', None) or {})
         headers.update(self._get_authentication_header())
         response = self.session.request(method=method, url=url, headers=headers, **kwargs)
         return self._process_response(response)
 
     def _get_authentication_header(self):
-        return {'Authorization', self.api_key}
+        return {'Authorization': self.api_key}
 
     def _process_response(self, response):
         response.raise_for_status()
         return response.json()
-
-    def get(self, **params):
-        params = dict(params)
-        url = self._get_url(params)
-        return self.request('get', url=url, params=params)
-
-    def _get_url(self, **params):
-        url = f'{self.base_url}{self.api_path}'
-        params_kw = {}
-        for k in list(params.keys()):
-            if "{key}".format(key=k) in url:
-                params_kw[k] = params.pop(k)
-        return url.format(**params_kw) if params_kw else url
--- pexels/test.py
+++ pexels/test.py
@@ -1,13 +1,47 @@
+from unittest import mock
+from unittest.mock import Mock
+
+from . import api
 from .api import Endpoint
 
 class TestEndpoint:
 
+    @staticmethod
+    def get_endpoint_stub(_api_path):
+        class Stub(Endpoint):
+            api_path = _api_path
+
+        return Stub
+
+    @staticmethod
+    def get_endpoint_stub_instance(_api_path):
+        StubCLass = TestEndpoint.get_endpoint_stub(_api_path)
+        return StubCLass(None)
+
+    def test_get_path_params_keys(self):
+        url = "http://domain.com/{categ_id}/{product_id}"
+        assert Endpoint._get_path_params_keys(url) == ['categ_id', 'product_id']
+
+    def test_remove_path_params(self):
+        params = dict(id=1000, per_page=10)
+        self.get_endpoint_stub(_api_path='photo/{id}')._remove_path_params(params) 
+        assert params == dict(per_page=10)
+
     def test_get_url(self):
-        endpoint = Endpoint(None)
-        endpoint.api_path = 'search'
-        assert endpoint._get_url() == f"{Endpoint.base_url}search"
+        assert self.get_endpoint_stub('search')._get_url() == f"{Endpoint.base_url}search"
 
     def test_get_url_with_path_params(self):
-        endpoint = Endpoint(None)
-        endpoint.api_path = 'photo/{id}'
-        assert endpoint._get_url(id=1000) == f"{Endpoint.base_url}photo/1000"
+        assert self.get_endpoint_stub(
+            _api_path='photo/{id}'
+        )._get_url(**dict(id='1000')) == f"{Endpoint.base_url}photo/1000"
+
+    @mock.patch.object(api.requests.Session, 'request')
+    def test_get(self, request_mock):
+        endpoint = self.get_endpoint_stub_instance(_api_path='photos/{id}')
+        endpoint.get(params=dict(id=1204, dummy='value'))
+        request_mock.assert_called_with(
+            method='get', 
+            url='https://api.pexels.com/v1/photos/1204', 
+            headers={'Accept-Content': 'application/json', 'Authorization': None}, 
+            params={'dummy': 'value'}
+        )
