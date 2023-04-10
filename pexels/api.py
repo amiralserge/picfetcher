2b051443a0ba3e49361496a2997213e18e98b7d4
amiralserge <kossononserge@yahoo.fr>
2023-04-10 00:55:07 -0400

    implement PhotoEndpoint

--- pexels/api.py
+++ pexels/api.py
@@ -5,3 +5,7 @@ from .base import BaseEndpoint
 class PhotoSearchEndpoint(BaseEndpoint):
     api_path = 'search'
     required_params = ['query']
+
+class PhotoEndpoint(BaseEndpoint):
+    api_path = 'photos/{id}'
+
