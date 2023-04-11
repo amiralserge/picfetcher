6c59133ad7073e5bb9e9ed7f4e301fdff1a91dd9
amiralserge <kossononserge@yahoo.fr>
2023-04-10 20:27:13 -0400

    implement Pexels class

--- pexels/v1.py
+++ pexels/v1.py
@@ -37,3 +37,31 @@ class FeaturedCollectionsEndpoint(BaseEndpoint):
 
 class CollectionMediaEndpoint(BaseEndpoint):
     api_path = 'v1/collections/{id}'
+
+
+class Pexels:
+
+    @staticmethod
+    def init(api_key):
+        from . import Config
+        Config.api_key = api_key
+
+    def __init__(self, api_key=None) -> None:
+        self.api_key = api_key or self._get_global_api_key()
+
+        self.photo = PhotoEndpoint(self.api_key)
+        self.photo_search = PhotosSearchEndpoint(self.api_key)
+        self.curated_photos = CuratedPhotosEndpoint(self.api_key)
+
+        self.video = VideoEndpoint(self.api_key)
+        self.video_search = VideosSearchEndpoint(self.api_key)
+        self.popular_videos = PopularVideosEndpoint(self.api_key)
+        
+        self.collection_media = CollectionMediaEndpoint(self.api_key)
+        self.featured_collections = FeaturedCollectionsEndpoint(self.api_key)
+        self.my_collections = MyCollectionsEndpoint(self.api_key)
+
+    @classmethod
+    def _get_global_api_key(cls):
+        from . import Config
+        return Config.api_key
