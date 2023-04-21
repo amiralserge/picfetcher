0144deb89044d09ec2032ac7b7c72dd616de24bd
amiralserge <kossononserge@yahoo.fr>
2023-04-21 19:21:16 -0400

    implement Image downloader

--- pexels/services.py
+++ pexels/services.py
@@ -4,6 +4,7 @@ import requests
 import shutil
 
 from typing import Any
+from functools import partial
 from .helpers import ResourceRetriever
 
 
@@ -12,24 +13,26 @@ class BaseResourceDowloader(abc.ABC):
     def __init__(self, retriever: ResourceRetriever) -> None:
         self._retriever = retriever
 
-    def download(self, where_to='./') -> None:
+    def download(self, where_to='./', **kwargs) -> None:
         # create dest folder if not existing already
         dest_folder_exists = os.path.exists(where_to)
         if not dest_folder_exists:
             os.mkdir(where_to)
 
+        _process = partial(self._process_resource, **kwargs)
         try:
-            for filename, url in self._retriever.retrieve(self._process_resource):
-                if os.path.exists(filename):
+            for filename, url in self._retriever.retrieve(_process):
+                dest_file = os.path.join(where_to, filename)
+                if os.path.exists(dest_file):
                     continue
-                self._download_resource(url, filename)
+                self._download_resource(url, dest_file)
         finally:
             # delete the folder if it didn't exist before
             if not dest_folder_exists:
                 self._delete_folder_if_not_empty(where_to)
 
     @abc.abstractmethod
-    def _process_resource(self, res_data) -> Any:
+    def _process_resource(self, res_data, **kwargs) -> Any:
         raise NotImplementedError()
 
     def _donwload_resource(self, url, dest_filename) -> None:
@@ -39,3 +42,23 @@ class BaseResourceDowloader(abc.ABC):
     def _delete_folder_if_not_empty(self, folder_path) -> None:
         if not os.path.isfile(folder_path) and not len(os.listdir(folder_path)):
             os.remove(folder_path)
+
+
+class ImageDownloader(BaseResourceDowloader):
+    class Quality:
+        MEDIUM = 'medium'
+        ORIGINAL = 'original'
+        LARGE2X = 'large2x'
+        LARGE = 'large'
+        SMALL = 'small'
+        LANDSCAPE = 'landscape'
+        PORTRAIT = 'tiny'
+
+    def download(self, where_to='./', quality:Quality=Quality.ORIGINAL) -> None:
+        return super().download(where_to, quality=quality)
+
+    def _process_resource(self, res_data, **kwargs) -> Any:
+        quality = kwargs.get('quality')
+        image_url = res_data['src'][quality]
+        filename = image_url.split('?')[0].split('/')[-1]
+        return filename, image_url
